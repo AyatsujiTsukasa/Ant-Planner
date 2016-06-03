ad9c3891f25a98e3da21d75eba51755589a9e6ae
Wu Zefeng <flamesdesperado@gmail.com>
2016-06-03 20:56:40 +0800

    original
    Need to change all the ‘fixed’ in userhome.css into ‘relative’.

--- plan.js
+++ plan.js
@@ -1,4 +1,4 @@
-function getCookie(c_name){
+function getCookie (c_name) {
 	if (document.cookie.length>0){
 		c_start=document.cookie.indexOf(c_name + "=");
 		if (c_start!=-1){
@@ -13,61 +13,61 @@ function getCookie(c_name){
 	return "";
 }
 
-$(".importance").mousemove(function(event) {
-	var starWidth = $(this).width()/3;
-	var v = (event.pageX - $(this).offset().left)/starWidth;
-	if(v < 1){
-		$(this).removeClass("importance2").removeClass("importance3").addClass("importance1");
-		$(this).siblings()[0].value = 1;
-	} else if (v < 2){
-		$(this).removeClass("importance1").removeClass("importance3").addClass("importance2");
-		$(this).siblings()[0].value = 2;
-	} else {
-		$(this).removeClass("importance2").removeClass("importance1").addClass("importance3");
-		$(this).siblings()[0].value = 3;
-	}
-});
-
-$('.reminder').on("click", function() {
-    var ele = $(this).siblings()[0];
-    ele.value = 1 - ele.value;
-    if(ele.value === "1"){
-        $(this).removeClass("reminderDisabled").addClass("reminderEnabled");
-        $(this).modal();
+function importanceChange (event, ele) {
+    var starWidth = $(ele).width()/3;
+    var v = (event.pageX - $(ele).offset().left)/starWidth;
+    if(v < 1){
+        $(ele).removeClass("importance2").removeClass("importance3").addClass("importance1");
+        $(ele).siblings()[0].value = 1;
+    } else if (v < 2){
+        $(ele).removeClass("importance1").removeClass("importance3").addClass("importance2");
+        $(ele).siblings()[0].value = 2;
     } else {
-        $(this).removeClass("reminderEnabled").addClass("reminderDisabled");
+        $(ele).removeClass("importance2").removeClass("importance1").addClass("importance3");
+        $(ele).siblings()[0].value = 3;
     }
-});
+}
 
-$('.planTagHeader').on("click", function() {
-	var sib = $(this).parent().siblings();
-	if(/subHidden/.test(sib.attr("class"))){
-		sib.removeClass("subHidden");
-		$(this).children().removeClass("planTagHeaderIconDown").addClass("planTagHeaderIconUp");
-	} else {
-		sib.addClass("subHidden");
-		$(this).children().removeClass("planTagHeaderIconUp").addClass("planTagHeaderIconDown");
-	}
-});
+function toggleReminder (ele) {
+    var element = $(ele).siblings()[0];
+    element.value = 1 - element.value;
+    if(element.value === "1"){
+        $(ele).removeClass("reminderDisabled").addClass("reminderEnabled");
+    } else {
+        $(ele).removeClass("reminderEnabled").addClass("reminderDisabled");
+    }
+}
 
-$('.location').on("keyup", function () {
-	url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + $(this).children().val();
-	var xhr = new XMLHttpRequest();
-	var _this = $(this);
+function dropMenu (ele) {
+	var sib = $(ele).parent().siblings();
+    if(/subHidden/.test(sib.attr("class"))){
+        sib.removeClass("subHidden");
+        $(ele).children().removeClass("planTagHeaderIconDown").addClass("planTagHeaderIconUp");
+    } else {
+        sib.addClass("subHidden");
+        $(ele).children().removeClass("planTagHeaderIconUp").addClass("planTagHeaderIconDown");
+    }
+}
+
+function searchMap (ele) {
+    test = ele;
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + $(ele).children().val();
+    var xhr = new XMLHttpRequest();
+    var _ele = $(ele);
     xhr.onreadystatechange = function () {
-    	if (this.readyState == 4) {
-	        try {
-	            if(this.status == 0) {
-	                throw('Status = 0');
-	            }
-	            res = jQuery.trim(this.responseText).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-	            response = JSON.parse(res);
-	            var lst = response.results.slice(0, 5);
-	            _this.children()[1].innerHTML = lst.map(function(x){return mapchoice(x.formatted_address, x.geometry.location.lat, x.geometry.location.lng);}).join("");
-	        } catch (e) {
-	        	console.log(e);
-	    	}
-	    }
+        if (this.readyState == 4) {
+            try {
+                if(this.status == 0) {
+                    throw('Status = 0');
+                }
+                res = jQuery.trim(this.responseText).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+                response = JSON.parse(res);
+                var lst = response.results.slice(0, 5);
+                _ele.children()[1].innerHTML = lst.map(function(x){return mapchoice(x.formatted_address, x.geometry.location.lat, x.geometry.location.lng);}).join("");
+            } catch (e) {
+                console.log(e);
+            }
+        }
     }
     try {
         xhr.open("get", url, true);
@@ -76,7 +76,7 @@ $('.location').on("keyup", function () {
     catch(e){
         console.log(e);
     }
-});
+}
 
 function initializeMap(ele){
 	var mapOptions = {
@@ -92,90 +92,80 @@ function mapchoice(name, lat, lng) {
 	return "<li class='list-group-item'><div class='mapChoice', onclick=initializeMap(this) lat='" + lat + "', lng='" + lng + "'>" + name + "</div></li>";
 }
 
-$(".planDesc").on({
-    mouseenter: function(){
-        $(".planDesc").addClass("planDesc-active");
-        $(".planDesc-input").addClass("planDesc-active-input");
-    },
-    mouseleave: function(){
-        $(".planDesc").removeClass("planDesc-active");
-        $(".planDesc-input").removeClass("planDesc-active-input");
-    },
-});
+function planDescMouseenter(ele){
+    $(ele).addClass("planDesc-active");
+    $(ele).children().children().addClass("planDesc-active-input");
+}
+function planDescMouseleave(ele){
+    $(ele).removeClass("planDesc-active");
+    $(ele).children().children().removeClass("planDesc-active-input");
+}
 
-$(".tags").on({
-    mouseenter: function(){
-        $(".tags").addClass("tags-active");
-        $(".tags-input").addClass("tags-active-input");
-    },
-    mouseleave: function(){
-        $(".tags").removeClass("tags-active");
-        $(".tags-input").removeClass("tags-active-input");
-    },
-});
 
-$(".due").on({
-    mouseenter: function(){
-        $(".due").addClass("due-active");
-        $(".due-input").addClass("due-active-input");
-    },
-    mouseleave: function(){
-        $(".due").removeClass("due-active");
-        $(".due-input").removeClass("due-active-input");
-    },
-});
+function tagsMouseenter(ele){
+    $(ele).addClass("tags-active");
+    $(ele).children().children().addClass("tags-active-input");
+}
+function tagsMouseleave(ele){
+    $(ele).removeClass("tags-active");
+    $(ele).children().children().removeClass("tags-active-input");
+}
 
-$(".repeat").on({
-    mouseenter: function(){
-        $(".repeat").addClass("repeat-active");
-        $(".repeat-input").addClass("repeat-active-input");
-    },
-    mouseleave: function(){
-        $(".repeat").removeClass("repeat-active");
-        $(".repeat-input").removeClass("repeat-active-input");
-    },
-});
 
-$(".reminderRow").on({
-    mouseenter: function(){
-        $(".reminderTagIcon").addClass("reminderTagIcon-active");
-    },
-    mouseleave: function(){
-        $(".reminderTagIcon").removeClass("reminderTagIcon-active");
-    },
-});
+function dueMouseenter(ele){
+    $(ele).addClass("due-active");
+    $(ele).children().children().addClass("due-active-input");
+}
+function dueMouseleave(ele){
+    $(ele).removeClass("due-active");
+    $(ele).children().children().removeClass("due-active-input");
+}
 
-$(".url").on({
-    mouseenter: function(){
-        $(".url").addClass("url-active");
-        $(".url-input").addClass("url-active-input");
-    },
-    mouseleave: function(){
-        $(".url").removeClass("url-active");
-        $(".url-input").removeClass("url-active-input");
-    },
-});
 
-$(".locationRow").on({
-    mouseenter: function(){
-        $(".locationTagIcon").addClass("locationTagIcon-active");
-        $(".location-input").addClass("location-active-input");
-    },
-    mouseleave: function(){
-        $(".locationTagIcon").removeClass("locationTagIcon-active");
-        $(".location-input").removeClass("location-active-input");
-    },
-});
+function repeatMouseenter(ele){
+    $(ele).addClass("repeat-active");
+    $(ele).children().children().addClass("repeat-active-input");
+}
+function repeatMouseleave(ele){
+    $(ele).removeClass("repeat-active");
+    $(ele).children().children().removeClass("repeat-active-input");
+}
 
-$(".cancel").on("click", function() {
-    document.getElementById("planForm").reset();
-});
 
-$(".submit").on("click", function() {
-    document.getElementsByName('ownerId')[0].value = getCookie("id");
-    document.getElementById("planForm").submit();
-});
+function reminderRowMouseenter(ele){
+    $(ele).addClass("reminderTagIcon-active");
+}
+function reminderRowMouseleave(ele){
+    $(ele).removeClass("reminderTagIcon-active");
+}
 
-$(function () {
-     $('#datetimepicker').datetimepicker();
-});
+
+function urlMouseenter(ele){
+    $(ele).addClass("url-active");
+    $(ele).children().children().addClass("url-active-input");
+}
+function urlMouseleave(ele){
+    $(ele).removeClass("url-active");
+    $(ele).children().children().removeClass("url-active-input");
+}
+
+
+function locationRowMouseenter(ele){
+    $(ele).addClass("locationTagIcon-active");
+    $(ele).children().children().addClass("location-active-input");
+}
+function locationRowMouseleave(ele){
+    $(ele).removeClass("locationTagIcon-active");
+    $(ele).children().children().removeClass("location-active-input");
+}
+
+var test = undefined;
+function resetForm(ele) {
+    $(ele).parent().parent().parent()[0].reset();
+}
+
+function submitForm(ele) {
+    test = ele;
+    $(document.getElementsByName('ownerId')).val(getCookie("id"));
+    $(ele).parent().parent().parent()[0].submit();
+}
