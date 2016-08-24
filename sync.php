d03f6599c2b5e0db22c32005dd4064b12599143b
eeeeasy <shixiyue97@gmail.com>
2016-08-24 10:39:06 +0800

    Fix bugs (optiosn icon, default time for due, timezone)

--- sync.php
+++ sync.php
@@ -38,6 +38,7 @@ if($pw === $password){
 	if($tagFilter !== "All"){
 		$getPlans .= " and customTags='".$tagFilter."'";
 	}
+	date_default_timezone_set("Asia/Singapore");
 	$date = getdate();
 	$formattedDate = $date['year']."-".$date['mon']."-".$date['mday'];
 	$formattedDateTime = $formattedDate." ".$date['hours'].":".$date['minutes'].":".$date['seconds'];
